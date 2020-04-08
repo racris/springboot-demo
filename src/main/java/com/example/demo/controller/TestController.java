e74d1c0a36d09e48842cbbe4b184740b18802756
racris <625404504@qq.com>
2020-04-08 22:28:20 +0800

    demo

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
